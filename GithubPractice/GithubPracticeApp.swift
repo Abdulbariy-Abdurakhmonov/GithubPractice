f2311267bfd9fe3a023f653d56afe3ffd5e656a3
Abdulbariy-Abdurakhmonov <abdulboriy21business@gmail.com>
2025-05-17 21:52:34 +0300

    Initial Commit

---
+++ GithubPractice/GithubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubPracticeApp.swift
+//  GithubPractice
+//
+//  Created by Abdulboriy on 17/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
